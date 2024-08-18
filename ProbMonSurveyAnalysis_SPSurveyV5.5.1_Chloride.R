f4d25a7fe880efc83c39552713d0aef83246f50c
Mary Becker <mary.becker@ct.gov>
2024-08-18 13:08:13 -0400

    update to chloride plots

--- ProbMonSurveyAnalysis_SPSurveyV5.5.1_Chloride.R
+++ ProbMonSurveyAnalysis_SPSurveyV5.5.1_Chloride.R
@@ -19,8 +19,9 @@ cont_ests_p50 <- cont_ests$Pct[cont_ests$Pct$Statistic == "50Pct",]
 cont_mean     <- cont_ests$Mean
 
 d <- list(cont_ests_p50, cont_mean)
-l <- c("Median Concentration of Chloride \n in Connecticut Streams",
-       "Average Concentration of Chloride \n in Connecticut Streams")
+t <- c("Median", "Average")
+p <- c("Chloride (ppm)")
+l <- paste("Concentration of", p ,"\n in Connecticut Streams")
 i <- 1
 
 ggplot(d[[i]],aes(x = Subpopulation, y = Estimate))+
@@ -30,11 +31,11 @@ ggplot(d[[i]],aes(x = Subpopulation, y = Estimate))+
            fill = "#0D2D6C", 
            size = 0.3,
            width = 0.7) +
-  geom_errorbar(aes(ymin = LCB95Pct, ymax = UCB95Pct),
-                width = 0.2,
-                position = position_dodge(0.7)) +
-  labs(title = l[i], 
-       y = "Chloride (ppm)", 
+  # geom_errorbar(aes(ymin = LCB95Pct, ymax = UCB95Pct),
+  #               width = 0.2,
+  #               position = position_dodge(0.7)) +
+  labs(title = paste(t[i], l), 
+       y = p, 
        x = "5-Year Interval Statewide Stream Surveys") +
   annotate("text", 
            x = 1:4, 
@@ -49,5 +50,30 @@ ggplot(d[[i]],aes(x = Subpopulation, y = Estimate))+
         plot.title = element_text(hjust = 0.5),
         text=element_text(size=10,face="bold"))
 
-ggsave("results/chloride_median_2001_2020_errbar.png")
+ggsave(paste0("results/chloride_",t[i],"_2001_2020.png"))
+
+ggplot(d[[i]],aes(x = Subpopulation, y = Estimate, group = 1))+
+  geom_line(linetype = 2) +
+  geom_point(colour = "#0D2D6C", size = 3) +
+  ylim(0, max(d[[i]]$Estimate))+
+  labs(title = paste(t[i], l), 
+       y = p, 
+       x = "5-Year Interval Statewide Stream Surveys") +
+  annotate("text", 
+           x = 1:4, 
+           y = c(d[[i]]$Estimate[1] - 3, 
+                 d[[i]]$Estimate[2] - 3,
+                 d[[i]]$Estimate[3] - 3,
+                 d[[i]]$Estimate[4] - 3),
+           color = "black",
+           label = c(round(d[[i]]$Estimate[1], digits = 1), 
+                     round(d[[i]]$Estimate[2], digits = 1), 
+                     round(d[[i]]$Estimate[3], digits = 1), 
+                     round(d[[i]]$Estimate[4], digits = 1)))+
+  theme_bw()+
+  theme(legend.position ="none",
+        plot.title = element_text(hjust = 0.5),
+        text=element_text(size=10,face="bold"))
+
+ggsave(paste0("results/chloride_point_",t[i],"_2001_2020.png"))
 
